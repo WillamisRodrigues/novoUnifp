262e939ec126f5e86afdaed3cb64e76f6d343097
Glaydson Prado <glaydsonp@hotmail.com>
2019-08-14 15:45:29 -0300

    carne de pagamento feito

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,14 +11,83 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-            DB::table('users')->insert([
+        $date = date('Y-m-d H:i:s');
+        DB::table('users')->insert([
             'name' => 'unifp',
             'email' => 'unifp@gmail.com',
             'password' => bcrypt('123456'),
             'nivelAcesso' => 0,
             'unidadeEscolar' => 0,
             'nascimento' => '2023/10/10'
+        ]);
+        DB::table('escolaridade')->insert([
+            'Escolaridade' => '1º Ano Ensino Médio',
+            'created_at' => $date,
+            'updated_at' => $date
+        ]);
+        DB::table('como_conheceu')->insert([
+            'ComoConheceu' => 'Facebook',
+            'created_at' => $date,
+            'updated_at' => $date
+        ]);
+        DB::table('funcionario')->insert([
+            'Nome' => 'Vendendor Seed',
+            'Nascimento' => $date,
+            'EstadoCivil' => 'Solteiro',
+            'Celular' => '(00) 00000-0000',
+            'TelefoneFixo' => '(00) 0000-0000',
+            'Email' => 'vendedor@seed.com',
+            'Cargo' => 'Vendedor',
+            'Setor' => 'Seed',
+            'Inativo' => 'Nao',
+            'created_at' => $date,
+            'updated_at' => $date
+        ]);
+        DB::table('funcionario')->insert([
+            'Nome' => 'Professor Seed',
+            'Nascimento' => $date,
+            'EstadoCivil' => 'Solteiro',
+            'Celular' => '(00) 00000-0000',
+            'TelefoneFixo' => '(00) 0000-0000',
+            'Email' => 'professor@seed.com',
+            'Cargo' => 'Professor',
+            'Setor' => 'Seed',
+            'Inativo' => 'Nao',
+            'created_at' => $date,
+            'updated_at' => $date
+        ]);
+        DB::table('curso')->insert([
+            'nomeCurso' => 'Curso Seed',
+            'QtdeAulas' => 10,
+            'CargaHoraria' => 10,
+            'created_at' => $date,
+            'updated_at' => $date
+        ]);
+        DB::table('turma')->insert([
+            'Curso' => 'Curso Seed',
+            'NomeTurma' => 'Turma Seed',
+            'DiasDaSemana' => $date,
+            'Periodo' => 'Tarde',
+            'Horario' => '15:14:15',
+            'DataInicio' => $date,
+            'DataTermino' => $date,
+            'DuracaoAulas' => '123',
+            'Professor' => 'Professor Seed',
+            'Vagas' => 123,
+            'Cronograma' => '123',
+            'Status' => 'Ativa',
+            'created_at' => $date,
+            'updated_at' => $date
+        ]);
+        DB::table('formas_pagamento')->insert([
+            'QtdeParcelas' => 1,
+            'BrutoTotal' => 1,
+            'ParcelaBruta' => 1,
+            'DescontoPontualidade' => 1,
+            'ParcelaDescontoPontualidade' => 1,
+            'ValorTotalDesconto' => 1,
+            'created_at' => $date,
+            'updated_at' => $date
         ]);
     }
 }
--- resources/views/pdf/carne.blade.php
+++ resources/views/pdf/carne.blade.php
@@ -1,5 +1,4 @@
 @foreach ($alunos as $aluno)
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,56 +16,169 @@
             font-size: 0.8rem;
             padding: 2px 4px;
         }
+
+        table{
+            border: 2px solid black;
+        }
     </style>
 </head>
 
 <body>
-    <table class="width: 100%">
-        <tr>
-            <td colspan="7"><p class="text-center text-uppercase">Carnê de Pagamento</p></td>
-        </tr>
-        <tr>
-            <td colspan="2" style="text-align:center">Recibo do Sacado</td>
-            <td rowspan="6"></td>
-            <td colspan="3">Estabelecimento: {!! 'nome da unidade' !!}</td>
-            <td>Vencimento: 0{!! $aluno->Vencimento !!}/00/0000</td>
-        </tr>
-        <tr>
-            <td>Matrícula</td>
-            <td>{!! '000000 - nome do aluno' !!}</td>
-            <td colspan="3">Aluno: {!! '000000 - nome do aluno' !!}</td>
-            <td>Valor do Doc.: R$ {!! '00,00' !!}</td>
-        </tr>
-        <tr>
-            <td>Carnê/Folha</td>
-            <td>Nª. 000000</td>
-            <td>Multa por atraso<br> <span class="text-right">10%</span> </td>
-            <td>Data da Emissão<br> <span class="text-right">00/00/0000</span> </td>
-            <td>Controle </td>
-            <td>Parcela Nº.<br> <span class="text-right">01/18</span> </td>
-        </tr>
-        <tr>
-            <td>Vencimento</td>
-            <td>00/00/0000</td>
-            <td colspan="3">Intruções: cobrar R$0,30 juros ao dia</td>
-            <td>Carnê/Folha <br> <span class="text-right">Nº. 000000</span></td>
-        </tr>
-        <tr>
-            <td>Valor</td>
-            <td>R$ 00,00</td>
-
-        </tr>
-        <tr>
-            <td>Valor Cobrado</td>
-            <td></td>
-        </tr>
-        <tr>
-            <td>Data de Pgto:</td>
-            <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            </td>
-        </tr>
-    </table>
-
+    @for ($i = 1; $i <= 6; $i++)
+        <table class='width: 100%;'>
+            <tr>
+                <td colspan='7'><p class='text-center text-uppercase' style="font-weight: bolder; font-size:2rem">Carnê de Pagamento</p></td>
+            </tr>
+            <tr>
+                <td colspan='2' style='text-align:center; font-weight:bold'>Recibo do Sacado</td>
+                <td rowspan='7'></td>
+                <td colspan='3'><span style='font-weight:bold'>Estabelecimento:</span>  {!!'nome da unidade'!!} </td>
+                <td><span style='font-weight:bold'>Vencimento</span>: 0{!!$aluno->Vencimento!!}/00/0000</td>
+            </tr>
+            <tr>
+                <td class="font-weight-bold">Matrícula</td>
+                <td> {!! $aluno->id !!} - {!!$aluno->Nome !!}</td>
+                <td colspan='3'><span style='font-weight:bold'>Aluno</span>: {!! $aluno->id !!} - {!! $aluno->Nome !!}</td>
+                <td><span style='font-weight:bold'>Valor do Doc.</span>: R$  {!!'00,00'!!} </td>
+            </tr>
+            <tr>
+                <td class="font-weight-bold">Carnê/Folha</td>
+                <td>Nª. {!! '000000'!!}</td>
+                <td>Multa por atraso<br> <span class='text-right'>10%</span> </td>
+                <td>Data da Emissão<br> <span class='text-right'>00/00/0000</span> </td>
+                <td>Controle <br></td>
+                <td>Parcela Nº.<br> <span class='text-right'> {!!$i!!} /18</span> </td>
+            </tr>
+            <tr>
+                <td class="font-weight-bold">Vencimento</td>
+                <td>0{!! $aluno->Vencimento !!}/00/0000</td>
+                <td colspan='3'>Intruções: cobrar R$0,30 juros ao dia</td>
+                <td>Carnê/Folha <br> <span class='text-right'>Nº. {!! '000000'!!}</span></td>
+            </tr>
+            <tr>
+                <td class="font-weight-bold">Valor</td>
+                <td>R$ 00,00</td>
+                <td colspan='3'><span style="font-weight: bolder">Sacador</span>: {!! $aluno->Pagador !!}</td>
+                <td>Valor cobrado: </td>
+            </tr>
+            <tr>
+                <td class="font-weight-bold">Valor Cobrado</td>
+                <td></td>
+                <td colspan='3'><span style="font-weight: bolder">Via do estabelecimento</span></td>
+                <td>Data de pgto.: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="font-weight-bold">Data de Pgto</td>
+                <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                </td>
+                <td colspan='4'></td>
+            </tr>
+        </table>
+        <br>
+        <div style="border-top: dotted 2px #555555;"></div>
+        <br>
+        <table class='width: 100%;'>
+            <tr>
+                <td colspan='7'><p class='text-center text-uppercase' style="font-weight: bolder; font-size:2rem">Carnê de Pagamento</p></td>
+            </tr>
+            <tr>
+                <td colspan='2' style='text-align:center; font-weight:bold'>Recibo do Sacado</td>
+                <td rowspan='7'></td>
+                <td colspan='3'><span style='font-weight:bold'>Estabelecimento:</span>  {!!'nome da unidade'!!} </td>
+                <td><span style='font-weight:bold'>Vencimento</span>: 0{!!$aluno->Vencimento!!}/00/0000</td>
+            </tr>
+            <tr>
+                <td class="font-weight-bold">Matrícula</td>
+                <td> {!! $aluno->id !!} - {!!$aluno->Nome !!}</td>
+                <td colspan='3'><span style='font-weight:bold'>Aluno</span>: {!! $aluno->id !!} - {!! $aluno->Nome !!}</td>
+                <td><span style='font-weight:bold'>Valor do Doc.</span>: R$  {!!'00,00'!!} </td>
+            </tr>
+            <tr>
+                <td class="font-weight-bold">Carnê/Folha</td>
+                <td>Nª. {!! '000000'!!}</td>
+                <td>Multa por atraso<br> <span class='text-right'>10%</span> </td>
+                <td>Data da Emissão<br> <span class='text-right'>00/00/0000</span> </td>
+                <td>Controle <br></td>
+                <td>Parcela Nº.<br> <span class='text-right'> {!!$i+1!!} /18</span> </td>
+            </tr>
+            <tr>
+                <td class="font-weight-bold">Vencimento</td>
+                <td>0{!! $aluno->Vencimento !!}/00/0000</td>
+                <td colspan='3'>Intruções: cobrar R$0,30 juros ao dia</td>
+                <td>Carnê/Folha <br> <span class='text-right'>Nº. {!! '000000'!!}</span></td>
+            </tr>
+            <tr>
+                <td class="font-weight-bold">Valor</td>
+                <td>R$ 00,00</td>
+                <td colspan='3'><span style="font-weight: bolder">Sacador</span>: {!! $aluno->Pagador !!}</td>
+                <td>Valor cobrado: </td>
+            </tr>
+            <tr>
+                <td class="font-weight-bold">Valor Cobrado</td>
+                <td></td>
+                <td colspan='3'><span style="font-weight: bolder">Via do estabelecimento</span></td>
+                <td>Data de pgto.: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="font-weight-bold">Data de Pgto</td>
+                <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                </td>
+                <td colspan='4'></td>
+            </tr>
+        </table>
+        <br>
+        <div style="border-top: dotted 2px #555555;"></div>
+        <br>
+        <table class='width: 100%;'>
+            <tr>
+                <td colspan='7'><p class='text-center text-uppercase' style="font-weight: bolder; font-size:2rem">Carnê de Pagamento</p></td>
+            </tr>
+            <tr>
+                <td colspan='2' style='text-align:center; font-weight:bold'>Recibo do Sacado</td>
+                <td rowspan='7'></td>
+                <td colspan='3'><span style='font-weight:bold'>Estabelecimento:</span>  {!!'nome da unidade'!!} </td>
+                <td><span style='font-weight:bold'>Vencimento</span>: 0{!!$aluno->Vencimento!!}/00/0000</td>
+            </tr>
+            <tr>
+                <td class="font-weight-bold">Matrícula</td>
+                <td> {!! $aluno->id !!} - {!!$aluno->Nome !!}</td>
+                <td colspan='3'><span style='font-weight:bold'>Aluno</span>: {!! $aluno->id !!} - {!! $aluno->Nome !!}</td>
+                <td><span style='font-weight:bold'>Valor do Doc.</span>: R$  {!!'00,00'!!} </td>
+            </tr>
+            <tr>
+                <td class="font-weight-bold">Carnê/Folha</td>
+                <td>Nª. {!! '000000'!!}</td>
+                <td>Multa por atraso<br> <span class='text-right'>10%</span> </td>
+                <td>Data da Emissão<br> <span class='text-right'>00/00/0000</span> </td>
+                <td>Controle <br></td>
+                <td>Parcela Nº.<br> <span class='text-right'> {!!$i+2!!} /18</span> </td>
+            </tr>
+            <tr>
+                <td class="font-weight-bold">Vencimento</td>
+                <td>0{!! $aluno->Vencimento !!}/00/0000</td>
+                <td colspan='3'>Intruções: cobrar R$0,30 juros ao dia</td>
+                <td>Carnê/Folha <br> <span class='text-right'>Nº. {!! '000000'!!}</span></td>
+            </tr>
+            <tr>
+                <td class="font-weight-bold">Valor</td>
+                <td>R$ 00,00</td>
+                <td colspan='3'><span style="font-weight: bolder">Sacador</span>: {!! $aluno->Pagador !!}</td>
+                <td>Valor cobrado: </td>
+            </tr>
+            <tr>
+                <td class="font-weight-bold">Valor Cobrado</td>
+                <td></td>
+                <td colspan='3'><span style="font-weight: bolder">Via do estabelecimento</span></td>
+                <td>Data de pgto.: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="font-weight-bold">Data de Pgto</td>
+                <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                </td>
+                <td colspan='4'></td>
+            </tr>
+        </table>
+    @endfor
 </body>
 
 </html>
